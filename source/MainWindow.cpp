be0cf761e519614d5729e35d8d98a436474a6c84
Korostast <Korostast@gmail.com>
2021-11-29 00:36:31 +0300

    First sketch of the interface (chats page).

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -3,6 +3,12 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    // Test
+    auto *button = new QPushButton("qerwerw");
+    auto *button1 = new QPushButton("qerwerw");
+    ui->chats_layout->addWidget(button);
+    ui->chats_layout->addWidget(button1);
 }
 
 MainWindow::~MainWindow() {
