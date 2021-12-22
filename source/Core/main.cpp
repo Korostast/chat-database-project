7be73b4f02f00a9f27687b22714f9b284b3c30a3
Korostast <Korostast@gmail.com>
2021-12-22 12:23:04 +0300

    UI improvements.

--- source/Core/main.cpp
+++ source/Core/main.cpp
@@ -5,15 +5,15 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    QFile file(":QSS");
-    file.open(QFile::ReadOnly);
-    a.setStyleSheet(QLatin1String(file.readAll()));
-
     qInstallMessageHandler(myMessageOutput);
 
     QFontDatabase::addApplicationFont(":Segoe UI");
     QFontDatabase::addApplicationFont(":Segoe UI Bold");
     QFontDatabase::addApplicationFont(":Roboto");
+
+    QFile file(":QSS");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(QLatin1String(file.readAll()));
 
     MainWindow window;
     window.show();
